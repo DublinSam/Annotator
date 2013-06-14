67f67b5498c973391c7517f1204c3c95d49af8a6
Sam <satiricallaughter@gmail.com>
2013-06-14 16:51:12 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,11 @@ TweetApp::Application.routes.draw do
     resources :annotations
   end
 
+  resources :users
+
   root :to => 'home#index'
 
-  get "users/new"
-
-
   match '/signup', to: 'users#new'
-
   match '/help', to: 'static_pages#help'
   match '/about', to: "static_pages#about"
   match '/contact', to: "static_pages#contact"
