853d2c2377b83845bab770f0b848a2d709ba2045
Sam <satiricallaughter@gmail.com>
2013-06-15 22:34:51 +0100

    added name and language level

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130614232055) do
+ActiveRecord::Schema.define(:version => 20130615210929) do
 
   create_table "annotations", :force => true do |t|
     t.string   "browserUAString"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(:version => 20130614232055) do
     t.datetime "updated_at",            :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.string   "name"
+    t.integer  "language_level"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
